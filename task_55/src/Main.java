b266a297fa0c38a893b931344bc7cde2bd1daa4e
Denis Petropavlov <wonder1224@yandex.ru>
2023-08-13 14:24:28 +0300

    13.08.2023

---
+++ task_55/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] argv) throws IOException{
+        Scanner sc = new Scanner(new File("input.txt"));
+        int a= sc.nextInt(), b= sc.nextInt();
+        while(a != b){
+            if(a > b) a = a - b;
+            else b = b - a;
+        }
+        PrintWriter pw = new PrintWriter(new File("output.txt"));
+        pw.print(a);
+        pw.close();
+    }
+}
