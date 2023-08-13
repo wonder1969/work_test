7a34eb9f5c3bde2df0b62a4996fc01f5f00ba5be
Denis Petropavlov <wonder1224@yandex.ru>
2023-08-13 14:21:59 +0300

    13.08.2023

---
+++ task_11/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String number; // Число-разница
+        int interimNumber;
+        int differenceNumber;
+        String str;
+        FileReader file = new FileReader("input.txt"); // Считывание данных из файла
+        Scanner sc = new Scanner(file);
+        str = sc.nextLine(); // 1 цифра
+        interimNumber = Integer.valueOf(str);
+        differenceNumber = 9 - interimNumber; // Получаем 3 цифру
+        number = str + 9 + differenceNumber;
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(number);
+        fileOut.close();
+    }
+}
---
+++ task_28/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int node = 1;
+        String str;
+        FileReader file = new FileReader("input.txt"); // Считывание данных из файла
+        Scanner sc = new Scanner(file);
+        str = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        while(st.hasMoreTokens()){
+            node = node * (Integer.valueOf(st.nextToken()) - 1);
+        }
+        FileWriter fileOut = new FileWriter("output.txt");
+        fileOut.write(String.valueOf(node));
+        fileOut.close();
+    }
+}
